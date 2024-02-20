27c81f13176c34e32fbc8c88e743699887e86d01
Olatomiwa Bifarin <obifarin@yahoo.com>
2024-02-20 13:39:24 -0500

    new UI

--- app.py
+++ app.py
@@ -13,14 +13,23 @@ with st.sidebar:
 
 if choice == 'Introduction':
     st.subheader("Machine Learning-Enabled Renal Cell Carcinoma Status Prediction Using Urine-Based Metabolomics")
-    ...
-    # Introduction content remains the same
-    ...
+    
+    st.info("""This is an interactive portal accompanying the paper on kidney cancer biomarker discovery,
+    navigate to the App section for details.
+    """)
+
+    st.write("Click here to read the [paper](https://pubs.acs.org/doi/10.1021/acs.jproteome.1c00213)")
+    with st.expander("Read full paper abstract"):
+        st.write("Renal cell carcinoma (RCC) is diagnosed through expensive cross-sectional imaging, frequently followed by renal mass biopsy, which is not only invasive but also prone to sampling errors. Hence, there is a critical need for a noninvasive diagnostic assay. RCC exhibits altered cellular metabolism combined with the close proximity of the tumor(s) to the urine in the kidney, suggesting that urine metabolomic profiling is an excellent choice for assay development. Here, we acquired liquid chromatography–mass spectrometry (LC–MS) and nuclear magnetic resonance (NMR) data followed by the use of machine learning (ML) to discover candidate metabolomic panels for RCC. The study cohort consisted of 105 RCC patients and 179 controls separated into two subcohorts: the model cohort and the test cohort. Univariate, wrapper, and embedded methods were used to select discriminatory features using the model cohort. Three ML techniques, each with different induction biases, were used for training and hyperparameter tuning. Assessment of RCC status prediction was evaluated using the test cohort with the selected biomarkers and the optimally tuned ML algorithms. A seven-metabolite panel predicted RCC in the test cohort with 88% accuracy, 94% sensitivity, 85% specificity, and 0.98 AUC. Metabolomics Workbench Study IDs are ST001705 and ST001706.")
+    
+    
+    
+    st.image("data/image_abstract.png")
 
 if choice == "App":
     st.subheader("""This graph shows the seven biomarkers discovered in our published work with selected metadata.""")
     with st.expander("How to use"):
-        st.write("""On your sidebar, use the dropdown to select relevant RCC meta-data of your interest and the urine biomarkers. 
+        st.write("""On your sidebar, use the dropdown to select Patient ID you are searching, relevant RCC meta-data of your interest and the urine biomarkers. 
                  The Graph is interactive, hover your mouse on data points to see Patient ID. Metabolites abundances were log-transformed.""")
 
     # Import cleaned up logtransformed data
